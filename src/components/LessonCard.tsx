08e3958981678956e3e3e6ddcf21c8c43ecc5f00
Andrea Baldazzi <67961970+baldo3000@users.noreply.github.com>
2026-09-11 17:16:12 +0200

    Remove debug console logs from LessonCard
    Removed console log statements for cleaner code.

--- src/components/LessonCard.tsx
+++ src/components/LessonCard.tsx
@@ -26,11 +26,6 @@ export const LessonCard = ({ lesson, strings, effectiveAcademicYear }: LessonCar
     
     // Extract base module code (e.g. 12345_1 -> 12345)
     const baseCode = teachingCode.split('_')[0];
-
-    console.log('Not Splitted');
-    console.log(teachingCode);
-    console.log('Splitted');
-    console.log(baseCode);
     
     trovaInsegnamento(baseCode, teacher, effectiveAcademicYear)
       .then((link) => {
